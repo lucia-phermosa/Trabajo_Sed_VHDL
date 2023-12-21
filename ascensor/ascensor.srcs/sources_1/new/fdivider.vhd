1671320a048c1e0de8bdb86ec923e1f4e8fffee4
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2023-12-21 15:31:28 +0100

    Update fdivider.vhd

--- ascensor/ascensor.srcs/sources_1/new/fdivider.vhd
+++ ascensor/ascensor.srcs/sources_1/new/fdivider.vhd
@@ -5,30 +5,27 @@ use IEEE.numeric_std.all;
 entity fdivider is
   Port ( 
        CLK     : in std_logic;
-       PSE_FREQ      : in integer := 1000 ; -- Desired Hz -- Uncomment when using with receiving signal
-       PSE_OUT : out std_logic
+       frecuencia      : in integer := 1000 ; -- Frecuencia deseada
+       clock_out : out std_logic
   );
 end fdivider;
 
 architecture Behavioral of fdivider is
-constant kCLK_FREQ : integer := 100000000; --kHz To the package
---constant PSE_FREQ : integer := 8000; -- Desired Hz -- Comment when using input
-signal sPSE_COUNTER : integer := 0; 
-signal sPSE_CNT_MAX : integer := kCLK_FREQ / PSE_FREQ; --kHz To the package
-signal sPSE_OUT      : std_logic := '0';
+constant clk_frec : integer := 100000000; -- Frecuencia del reloj de la fpga (100MHz)
+signal count : integer := 0; 
+signal max : integer := clk_frec / frecuencia; 
+signal signal_out : std_logic := '0';
 
 begin
-
-PSE_OUT <= sPSE_OUT;
 process (CLK)
 begin
     if (rising_edge(CLK)) then
-        sPSE_COUNTER <= sPSE_COUNTER + 1;
-        if (sPSE_COUNTER > sPSE_CNT_MAX) then
-            sPSE_COUNTER <= 0;
-        elsif (sPSE_COUNTER = sPSE_CNT_MAX) then
-            sPSE_OUT <= not SPSE_OUT;
+        count <= count + 1;
+        if (count >= max) then
+            signal_out <= not signal_out;
+            count <= 1;
         end if;
     end if;
 end process ;
+clock_out <= signal_out;
 end Behavioral;
